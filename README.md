911d20ee073399036f4e8c621e6e56f49b52882d
Fikri Firman Fadilah <fikrifirmanfadilah@gmail.com>
2021-03-10 19:35:15 +0700

    update and adding license

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Documentation
+
+This is a Restful API about Journal to Journal Summary conversion created using **NodeJS**. Data source <https://getdigest.com/>.
+
+## Demo
+
+<#>
+Comming Soon
+
+## Installation
+
+```
+git clone https://github.com/fikrifirmanf/Journal-Summary-App-REST-API.git
+cd Journal-Summary-App-REST-API
+npm install
+```
+
+### Running on Dev Mode
+
+```
+npm run devStart
+```
+
+### Status Code
+
+#### Response Code
+
+```
+200: Success
+500: Internal Server Error
+```
+
+## REST API
+
+GET <http://localhost:3000/api/resume?url={pdf_url}> --> Getting result journal summary
+
+## Data Source
+
+<https://getdigest.com/>
+
+### NOT FOR COMMERCIAL
+
+[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
--- download_pdf.js
+++ download_pdf.js
@@ -2,25 +2,27 @@ const https = require('https')
 
 const getPdf = (url, callback) => {
     https.get(url, res => {
-        // Initialise an array
-        const bufs = [];
+            // Initialise an array
+            const bufs = [];
 
-        // Add the data to the buffer collection
-        res.on('data', function (chunk) {
-            bufs.push(chunk)
-        });
+            // Add the data to the buffer collection
+            res.on('data', function (chunk) {
+                bufs.push(chunk)
+            });
 
-        // This signifies the end of a request
-        res.on('end', function () {
-            // We can join all of the 'chunks' of the image together
-            const data = Buffer.concat(bufs);
+            // This signifies the end of a request
+            res.on('end', function () {
+                // We can join all of the 'chunks' of the image together
+                const data = Buffer.concat(bufs);
 
-            // Then we can call our callback.
-            callback(null, data);
-        });
-    })
-    // Inform the callback of the error.
-    .on('error', callback);
+                // Then we can call our callback.
+                callback(null, data);
+            });
+        })
+        // Inform the callback of the error.
+        .on('error', callback);
 }
 
-module.exports = {getPdf}
+module.exports = {
+    getPdf
+}
